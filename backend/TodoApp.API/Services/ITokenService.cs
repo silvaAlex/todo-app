cbcfcd07ebdcf3a3750170eb103066399c6c94cb
Alex Junior <silva_jralex@hotmail.com>
2026-01-28 16:21:15 -0300

    feat: add JWT Autheticated

---
+++ backend/TodoApp.API/Services/ITokenService.cs
@@ -0,0 +1,8 @@
+﻿using TodoApp.Api.Models;
+
+namespace TodoApp.API.Services;
+
+public interface ITokenService
+{
+    string GenerateToken(User user);
+}
---
+++ backend/TodoApp.API/Services/TokenService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TodoApp.Api.Models;
+using TodoApp.API.Infra.Auth;
+
+namespace TodoApp.API.Services;
+
+public class TokenService(IOptions<JWTSettings> settings) : ITokenService
+{
+    private readonly JWTSettings _jwtSettings = settings.Value;
+
+    public string GenerateToken(User user)
+    {
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName!),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+
+        var creditials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(_jwtSettings.ExpirationMinutes),
+            signingCredentials: creditials        
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
