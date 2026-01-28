d234200048a67a00976c4f052c5fc2d9b16a0867
Alex Junior <silva_jralex@hotmail.com>
2026-01-28 16:23:09 -0300

    feat: add AuthController

---
+++ backend/TodoApp.API/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApp.API.DTOs;
+using TodoApp.API.Notifications;
+using TodoApp.API.UseCases;
+
+namespace TodoApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController(IUserService userService, DomainNotifier notifier) : ControllerBase
+    {
+        private readonly IUserService _userService = userService;
+        private readonly DomainNotifier _notifier = notifier;
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserCreateDto userDto)
+        {
+            var result = await _userService.RegisterAsync(userDto);
+
+            if(_notifier.HasNotifications)
+                return BadRequest(_notifier.Notifications);
+
+            return Ok(result);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginDto userDto)
+        {
+            var result = await _userService.LoginAsync(userDto);
+
+            if (_notifier.HasNotifications)
+                return Unauthorized(_notifier.Notifications);
+
+            return Ok(result);
+        }
+    }
+}
