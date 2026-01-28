fbf86d96322e138cb1d0796593c85fc8c3215196
Alex Junior <silva_jralex@hotmail.com>
2026-01-28 00:41:29 -0300

    feat: add TodoAppDbContext with DbSets and relationships

---
+++ backend/TodoApp.API/Data/TodoAppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Api.Models;
+using Task = TodoApp.Api.Models.Task;
+
+namespace TodoApp.API.Data;
+
+public class TodoAppDbContext(DbContextOptions<TodoAppDbContext> options) : DbContext(options)
+{
+    public DbSet<Task> Tasks {  get; set; }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Task>()
+            .HasOne(t => t.User)
+            .WithMany(u => u.Tasks)
+            .HasForeignKey(t => t.UserId);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
