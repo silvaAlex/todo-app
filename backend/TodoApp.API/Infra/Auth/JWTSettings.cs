0691998082bc08889350780643391ffe0bfaa6eb
Alex Junior <silva_jralex@hotmail.com>
2026-01-28 16:22:19 -0300

    feat: add JWTSettings

---
+++ backend/TodoApp.API/Infra/Auth/JWTSettings.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace TodoApp.API.Infra.Auth;
+
+public class JWTSettings
+{
+    public string SecretKey { get; set; } = string.Empty;
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+    public int ExpirationMinutes { get; set; }
+}
+
