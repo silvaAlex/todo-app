0daa042f3c8f271bdc907edbdb1926095fe174e8
Alex Junior <silva_jralex@hotmail.com>
2026-01-28 12:30:03 -0300

    chore: add DI registration for Repositories, UseCases

---
+++ backend/TodoApp.API/Configuration/ServicesCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using TodoApp.API.Notifications;
+using TodoApp.API.Repositories;
+using TodoApp.API.UseCases;
+
+namespace TodoApp.API.Configuration;
+public static class ServicesCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<DomainNotifier>();
+
+        services.AddScoped<IUserRepository,  UserRepository>();
+        services.AddScoped<ITaskRepository, TaskRepository>();
+        services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<ITaskService, TaskService>();
+
+        return services;
+    }
+}
