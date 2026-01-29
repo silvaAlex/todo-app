b964a9c61c9d6cbf2295658b5c9b78eecc0de12b
Alex Junior <silva_jralex@hotmail.com>
2026-01-29 12:22:14 -0300

    feat: add frontend services for API integration

---
+++ frontend/ToDoApp.Web/Services/AuthService.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Web.Client;
+using ToDoApp.Web.Models.Auth;
+using ToDoApp.Web.Models.Register;
+
+namespace ToDoApp.Web.Services
+{
+    public class AuthService
+    {
+        public async Task<string> LoginAsync(LoginRequest request)
+        {
+            var api = new ApiClient();
+
+            var jsonContent = JsonConvert.SerializeObject(request);
+
+            var content = new StringContent(
+                jsonContent,
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            var response = await api.Client.PostAsync("api/auth/login", content).ConfigureAwait(false);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var erroContent = await response.Content.ReadAsStringAsync();
+                throw new System.Exception($"Status: {response.StatusCode}, Erro: {erroContent}");
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<LoginResponse>(json);
+
+            return result.Token;
+        }
+
+        public async Task<bool> CreateAsync(RegisterRequest request)
+        {
+            var api = new ApiClient();
+
+            var json = JsonConvert.SerializeObject(request);
+
+            var content = new StringContent(
+                json,
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            var response = await api.Client.PostAsync("api/auth/register", content);
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ frontend/ToDoApp.Web/Services/TaskService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using ToDoApp.Web.Client;
+using ToDoApp.Web.Models.Tasks;
+
+namespace ToDoApp.Web.Pages
+{
+    public class TaskService
+    {
+        private ApiClient _api;
+        public TaskService()
+        {
+            _api = new ApiClient();
+            var token = Helpers.AuthSession.GetToken();
+            _api.SetAuthToken(token);
+        }
+
+        public async Task<List<TaskResponse>> GetAllTasksAsync()
+        {
+            var response = await _api.Client.GetAsync("api/tasks").ConfigureAwait(false);
+
+            if (!response.IsSuccessStatusCode)
+                return new List<TaskResponse>();
+            return await response.Content.ReadFromJsonAsync<List<TaskResponse>>() ?? new List<TaskResponse>();
+        }
+
+        public async Task<TaskResponse> GetTasksByIdAsync(Guid id)
+        {
+            var response = await _api.Client.GetAsync($"api/tasks/{id}");
+
+            if (!response.IsSuccessStatusCode)
+                return null;
+
+            return await response.Content.ReadFromJsonAsync<TaskResponse>();
+        }
+
+        public async Task<TaskResponse> CreateTaskAsync(TaskRequest request)
+        {
+            var response = await _api.Client.PostAsJsonAsync($"api/tasks", request);
+
+            if (!response.IsSuccessStatusCode)
+                return null;
+
+            return await response.Content.ReadFromJsonAsync<TaskResponse>();
+        }
+
+        public async Task<TaskResponse> UpdateTaskAsync(Guid id, TaskRequest request)
+        {
+            var response = await _api.Client.PutAsJsonAsync($"api/tasks/{id}", request);
+
+            if (!response.IsSuccessStatusCode)
+                return null;
+
+            return await response.Content.ReadFromJsonAsync<TaskResponse>();
+        }
+
+        public async Task<bool> CompleteTaskAsync(Guid id)
+        {
+            var response = await _api.Client.PatchAsJsonAsync($"api/tasks/{id}", "");
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeletTaskAsync(Guid id)
+        {
+            var response = await _api.Client.DeleteAsync($"api/tasks/{id}");
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<List<TaskResponse>> GetTasksByCategoryAsync(string category)
+        {
+            var response = await _api.Client.GetAsync($"api/tasks/category/{category}");
+
+            if (!response.IsSuccessStatusCode)
+                return new List<TaskResponse>();
+            return await response.Content.ReadFromJsonAsync<List<TaskResponse>>() ?? new List<TaskResponse>();
+        }
+    }
+}
