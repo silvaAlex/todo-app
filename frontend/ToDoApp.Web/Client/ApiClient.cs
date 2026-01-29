e0175a8c930fe3349bbe32af5fafea503ed1ee79
Alex Junior <silva_jralex@hotmail.com>
2026-01-29 12:18:04 -0300

    chore: add ApiClient for API communication with JWT bearer support

---
+++ frontend/ToDoApp.Web/Client/ApiClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ToDoApp.Web.Client
+{
+    public class ApiClient
+    {
+        private static readonly Lazy<HttpClient> _lazyClient = new Lazy<HttpClient>(() =>
+        {
+            var baseUrl = ConfigurationManager.AppSettings["ApiBaseUrl"];
+
+            if (string.IsNullOrEmpty(baseUrl))
+                throw new ConfigurationErrorsException("ApiBaseUrl não foi configurada no Web.Config");
+
+            return new HttpClient
+            {
+                BaseAddress = new Uri(baseUrl),
+                Timeout = TimeSpan.FromSeconds(30)
+            };
+        });
+
+        public HttpClient Client = _lazyClient.Value;
+
+        public void SetAuthToken(string token)
+        {
+            if (string.IsNullOrEmpty(token))
+                Client.DefaultRequestHeaders.Authorization = null;
+            else
+                Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+    }
+}
